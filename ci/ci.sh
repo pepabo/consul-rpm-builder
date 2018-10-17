2026df6eb9abcf4f63a3f24f684150c38f2fdc80
r_takaishi <ryo.takaishi.0@gmail.com>
2018-10-17 11:38:10 +0900

    VERSION to env

--- ci/ci.sh
+++ ci/ci.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-VERSION=0.8.4
-
 make base
 docker build --no-cache -t consul-rpm-builder/consul:${VERSION} consul/${VERSION}
 docker run consul-rpm-builder/consul:${VERSION} > consul-${VERSION}.x86_64.rpm
